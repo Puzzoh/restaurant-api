dd7607fec98aba836373502e9044ff6bc1d0d21f
mphung1 <pgminh317@gmail.com>
2023-06-10 22:45:04 -0700

    add voucher codes generator

---
+++ voucherify.go
@@ -0,0 +1,26 @@
+package voucherify
+
+// input configuration.Config
+func Generate(input string) []string {
+	if len(input) == 1 {
+		return []string{input}
+	}
+
+	runes := []rune(input)
+	subPermutations := Generate(string(runes[0 : len(input)-1]))
+
+	result := []string{}
+	for _, s := range subPermutations {
+		result = append(result, merge([]rune(s), runes[len(input)-1])...)
+	}
+
+	return result
+}
+
+// Insert a char in a word
+func merge(ins []rune, c rune) (result []string) {
+	for i := 0; i <= len(ins); i++ {
+		result = append(result, string(ins[:i])+string(c)+string(ins[i:]))
+	}
+	return
+}
